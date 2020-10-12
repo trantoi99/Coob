1b9c9e178f3c7cca3287fa5aa39d506719a1802f
trantoi99 <tranhungtoi308@gmail.com>
2020-10-13 00:50:18 +0700

    aaaaaaa

--- app/js/script.js
+++ app/js/script.js
@@ -18,3 +18,18 @@ let x = setInterval(function() {
     document.getElementById('minute').innerText = minutes;
     document.getElementById('second').innerText = seconds;
 }, 1000);
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('theInput').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('theInput').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('theInput').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('theInput').value = value;
+}
