f4ad7e72206c27cbb58f722294b0353830e6e321
trantoi99 <tranhungtoi308@gmail.com>
2020-10-13 16:28:08 +0700

    no message

--- app/js/script.js
+++ app/js/script.js
@@ -30,6 +30,25 @@ $(document).ready(function() {
             }
         ]
     });
+    const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+    accordionItemHeaders.forEach(accordionItemHeader => {
+        accordionItemHeader.addEventListener("click", event => {
+            const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+            /* dùng mở một faqs
+             if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+                 currentlyActiveAccordionItemHeader.classList.toggle("active");
+                 currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+             }
+             */
+            accordionItemHeader.classList.toggle("active");
+            const accordionItemBody = accordionItemHeader.nextElementSibling;
+            if (accordionItemHeader.classList.contains("active")) {
+                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+            } else {
+                accordionItemBody.style.maxHeight = 0;
+            }
+        });
+    });
 });
 let countDown = new Date('December 31, 2020 00:00:00').getTime();
 let now = new Date().getTime();
